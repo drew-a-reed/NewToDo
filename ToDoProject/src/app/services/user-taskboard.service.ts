64bd47ac8d1f367567bb3227b2f3a86d7ab24e2d
Drew A Reed <drew.reed@hotmail.com>
2024-10-25 11:19:42 -0500

    Update user-taskboard.service.ts

--- ToDoProject/src/app/services/user-taskboard.service.ts
+++ ToDoProject/src/app/services/user-taskboard.service.ts
@@ -14,15 +14,15 @@ export class UserTaskboardService {
 
   addUserToTaskboard(userTaskboard: any) {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.post<any>(`${this.baseUrl}`, userTaskboard, { headers });
+    return this.http.post<any>(`${this.baseUrlDev}`, userTaskboard, { headers });
   }
 
   getTaskboardUsers(taskboardId: any){
-    return this.http.get<any>(`${this.baseUrl}${taskboardId}/users`)
+    return this.http.get<any>(`${this.baseUrlDev}${taskboardId}/users`)
   }
 
   getUserTaskboards(userId: any){
-    return this.http.get<any>(`${this.baseUrl}${userId}/taskboards`);
+    return this.http.get<any>(`${this.baseUrlDev}${userId}/taskboards`);
   }
 
 }
